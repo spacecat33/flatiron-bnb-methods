c1704c32f7b2ac6285605fadb794613aaee6bc74
Janan Akkad <jananakkad@hotmail.co.uk>
2022-11-13 15:17:02 +0000

    Done.

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,19 @@
 class Neighborhood < ActiveRecord::Base
   belongs_to :city
   has_many :listings
+  
+  include Reservable
+  extend Reservable::ClassMethods
+  # The same class/instance methods as your City object. Maybe there is a way they can share code?!?!?
+  # instance methods
+
+
+  def neighborhood_openings(begin_date, end_date)
+    self.listings.collect do |listing|
+      if !listing.listing_reserved?(begin_date, end_date)
+        listing
+      end
+    end
+  end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,20 @@
 class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
+  
+  
+  validates :description, presence: true
+  validates :rating, presence: true
+  validates :reservation, presence: true
+  validate :res_passed
 
+  private
+
+  def res_passed
+    if reservation
+      if self.reservation.checkout > Date.today
+        errors.add(:reservation, "did not pass")
+      end
+    end
+  end
 end
